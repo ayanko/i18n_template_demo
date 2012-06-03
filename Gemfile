d3eed718638e451b52ebb34b3c0f026d3bea777b
Andriy Yanko <andriy.yanko@gmail.com>
2012-06-04 02:29:53 +0300

    Initial

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem "sinatra", "1.3.2"
+
+gem "i18n_template", 
+  :git => "git://github.com/railsware/i18n_template.git",
+  :branch => "development"
---
+++ application.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'i18n_template'
+
+class Application < Sinatra::Base
+  get '/' do
+    internationalize
+  end
+
+  post '/' do
+    internationalize
+  end
+
+  include ERB::Util
+
+  def internationalize
+    @source = params[:source] || default_source
+
+    document = I18nTemplate::Document.new(@source)
+    document.process!
+
+    @template = document.source
+    @phrases = document.keys.uniq.join("\n")
+
+    erb :index
+  end
+
+  def default_source
+    <<-DATA
+<body>
+  <h2>Hello World</h2>
+  <%= link_to "Home", home_path %>
+</body>
+    DATA
+  end
+end
